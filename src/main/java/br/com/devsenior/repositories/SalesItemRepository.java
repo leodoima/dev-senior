45fe82c76acc8fdbcc7a18ad920a5d29af7b9442
Leonardo dos Santos <leodoima@hotmail.com>
2022-10-02 18:22:48 -0300

    Configurações finais da aplicação

--- src/main/java/br/com/devsenior/repositories/SalesItemRepository.java
+++ src/main/java/br/com/devsenior/repositories/SalesItemRepository.java
@@ -2,8 +2,16 @@ package br.com.devsenior.repositories;
 
 import br.com.devsenior.models.SalesItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface SalesItemRepository extends JpaRepository<SalesItem, UUID> {
+
+    @Query("SELECT sum(amount * unitaryValue) FROM SalesItem WHERE salesOrder = :id AND product = 'SERVICE'")
+    public double sumServiceInOrder(@Param("id") UUID id);
+
+    @Query("SELECT sum(amount * unitaryValue) FROM SalesItem WHERE salesOrder = :id AND product = 'PRODUCT'")
+    public double sumProductInOrder(@Param("id") UUID id);
 }
